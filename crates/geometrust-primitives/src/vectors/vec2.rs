05e726d277832d9afc1bf1acff5ed8d3cc0bb174
Samuel McNally <sammcnally587@gmail.com>
2026-09-24 18:35:42 -0500

    make methods consume &self rather than self

--- crates/geometrust-primitives/src/vectors/vec2.rs
+++ crates/geometrust-primitives/src/vectors/vec2.rs
@@ -18,49 +18,49 @@ impl Vec2F64 {
 
     /// Add a value to the x component of the vector.
     #[inline]
-    pub const fn add_x(self, x: f64) -> Self {
+    pub const fn add_x(&self, x: f64) -> Self {
         Self::new(self.x + x, self.y)
     }
 
     /// Add a value to the y component of the vector.
     #[inline]
-    pub const fn add_y(self, y: f64) -> Self {
+    pub const fn add_y(&self, y: f64) -> Self {
         Self::new(self.x, self.y + y)
     }
 
     /// Multiply the x component of the vector by a value.
     #[inline]
-    pub const fn scale_x(self, x: f64) -> Self {
+    pub const fn scale_x(&self, x: f64) -> Self {
         Self::new(self.x * x, self.y)
     }
 
     /// Multiply the y component of the vector by a value.
     #[inline]
-    pub const fn scale_y(self, y: f64) -> Self {
+    pub const fn scale_y(&self, y: f64) -> Self {
         Self::new(self.x, self.y * y)
     }
 
     /// Subtract the x component of the vector by a value.
     #[inline]
-    pub const fn sub_x(self, x: f64) -> Self {
+    pub const fn sub_x(&self, x: f64) -> Self {
         Self::new(self.x - x, self.y)
     }
 
     /// Subtract the y component of the vector by a value.
     #[inline]
-    pub const fn sub_y(self, y: f64) -> Self {
+    pub const fn sub_y(&self, y: f64) -> Self {
         Self::new(self.x, self.y - y)
     }
 
     /// Divide the x component of the vector by a value.
     #[inline]
-    pub const fn div_x(self, x: f64) -> Self {
+    pub const fn div_x(&self, x: f64) -> Self {
         Self::new(self.x / x, self.y)
     }
 
     /// Divide the y component of the vector by a value.
     #[inline]
-    pub const fn div_y(self, y: f64) -> Self {
+    pub const fn div_y(&self, y: f64) -> Self {
         Self::new(self.x, self.y / y)
     }
 }
